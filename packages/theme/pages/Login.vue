321703ac213276c8eae18caa6dbcc61f01acf785
Ujjwal <shyamkumartiwari405@gmail.com>
2022-06-07 11:06:24 +0530

    Change :-
    Added login functionality with email and password through firebase

--- packages/theme/pages/Login.vue
+++ packages/theme/pages/Login.vue
@@ -15,10 +15,16 @@
       </h3>
       <h4>for All</h4>
       <div class="traditional-login-container" style="width: 60%;">
-          <SfInput v-model="loginEsmail" :type="'text'" :label="'Email*'" />
-          <SfInput label="Password*" type="password"
-            :icon='{"icon":"show_password","color":"black","size":"18px"}' valid hasShowPassword />
-          <SfButton type="button" @click="userSignIn">Sign In</SfButton>
+        <SfInput v-model="auth.email" :type="'text'" :label="'Email*'" />
+        <SfInput
+          label="Password*"
+          type="password"
+          v-model="auth.password"
+          :icon="{ icon: 'show_password', color: 'black', size: '18px' }"
+          valid
+          hasShowPassword
+        />
+        <SfButton type="button" @click="login">Sign In</SfButton>
       </div>
     </div>
     <div></div>
@@ -45,22 +51,25 @@ export default {
   },
   data() {
     return {
-      loginEsmail: '',
-      showPwd: true
+      showPwd: true,
+      auth: {
+        email: '',
+        password: ''
+      }
     };
   },
   methods: {
-    userSignIn() {
-      // function ValidateEmail(mail) {
-      /* eslint-disable */
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.loginEsmail)) {
-        alert('User Signed In successful !');
-        } else {
-        // }
-        alert("Incorrect email id patern")
-        }
-        /* eslint-enable */
+    login() {
+      this.$fire.auth
+        .signInWithEmailAndPassword(this.auth.email, this.auth.password)
+        .catch(function(error) {
+          alert(error.message);
+        })
+        .then((user) => {
+          $nuxt.$router.push('/');
+        });
     },
+
     facebookLogin() {
       /* eslint-disable no-undef */
 
@@ -98,70 +107,70 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .top-bar {
-    align-items: center;
-    display: flex;
-    font-size: 18px;
-    justify-content: space-around;
-    height: 60px;
-    font-weight: 500;
-    background: white;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.07);
+.top-bar {
+  align-items: center;
+  display: flex;
+  font-size: 18px;
+  justify-content: space-around;
+  height: 60px;
+  font-weight: 500;
+  background: white;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.07);
 
-    .icon_back {
-      position: absolute;
-      left: 0;
-      margin: 10px;
-    }
+  .icon_back {
+    position: absolute;
+    left: 0;
+    margin: 10px;
+  }
+}
+
+.open-search {
+  @media (min-width: 560px) {
+    padding-top: 40px;
+    width: 50%;
+    margin: auto;
   }
 
-  .open-search {
-    @media (min-width: 560px) {
-      padding-top: 40px;
-      width: 50%;
-      margin: auto;
-    }
+  padding: 40px 28px;
 
-    padding: 40px 28px;
-
-    h3 {
-      font-size: 40px;
-      font-weight: 800;
-      color: #f37a20;
-      line-height: 45px;
-    }
-
-    h4 {
-      font-size: 27px;
-      font-weight: 800;
-      line-height: 30px;
-    }
-
-    .socialMedia-login-container {
-      padding-top: 41px;
-    }
-
-    .span-text {
-      padding-right: 225px;
-    }
-
-    .social-span-content {
-      position: absolute;
-    }
-
-    .google-button {
-      width: 300px;
-      text-transform: capitalize;
-      border-radius: 6px;
-      background: #5383ec;
-    }
-
-    .facebook-button {
-      width: 300px;
-      margin-top: 30px;
-      text-transform: capitalize;
-      border-radius: 6px;
-      background: #4a66ac;
-    }
+  h3 {
+    font-size: 40px;
+    font-weight: 800;
+    color: #f37a20;
+    line-height: 45px;
   }
+
+  h4 {
+    font-size: 27px;
+    font-weight: 800;
+    line-height: 30px;
+  }
+
+  .socialMedia-login-container {
+    padding-top: 41px;
+  }
+
+  .span-text {
+    padding-right: 225px;
+  }
+
+  .social-span-content {
+    position: absolute;
+  }
+
+  .google-button {
+    width: 300px;
+    text-transform: capitalize;
+    border-radius: 6px;
+    background: #5383ec;
+  }
+
+  .facebook-button {
+    width: 300px;
+    margin-top: 30px;
+    text-transform: capitalize;
+    border-radius: 6px;
+    background: #4a66ac;
+  }
+}
 </style>
